6ed41cfe1f44ccac01676d41552a6ae71a697b67
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2020-10-21 14:26:33 +0300

    Начали делать вывод конкретного упражнения

--- view/category.php
+++ view/category.php
@@ -1,12 +1,9 @@
-    
 <?php
 class ViewCategories {
 
     public static function AllCategories($arr) {
         foreach ($arr as $value) {
-            echo "<ul class='submenu'>";
             echo "<li><a href='category?id=".$value['id']."'>".$value['title'].'</a></li><br>';
-            echo "</ul>";
         }
     }
 }
--- view/layout2.php
+++ view/layout2.php
@@ -1,4 +1,10 @@
-
+<html>
+    <head>
+        <title>menu</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">-->
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         
     </head>
   <body>  
@@ -7,14 +13,14 @@
     </div>
     <section>
          <div class="divBox">
-             
+             <ul class="submenu">
                  <?php
                      if(isset($content)){
                          echo $content;
                      }
                      else {echo '<h1>Content is gone!</h1>';}
              ?>
-                
+                 </ul>
          </div>
     </section>
         
