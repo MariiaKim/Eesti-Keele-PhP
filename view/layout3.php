2c4955028028deb1c687f157fc6a39be92cf330a
pupil <pupil@ivkhk.ee>
2020-11-05 15:17:31 +0200

    поправила стили (маша)

--- view/layout3.php
+++ view/layout3.php
@@ -3,7 +3,7 @@
         <title>Exercise</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="style2.css">
     </head>
     <body>
         <p class="vih"><a href="./"><img src="image/7.png"></a></p>
